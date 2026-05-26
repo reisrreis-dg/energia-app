f57de79da7fd60b4f713843cbd4c9f67689fc034
reisrreis-dg <reis.rreis@gmail.com>
2026-05-26 14:07:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,21 @@ function calcular() {
     const valorCasa1 = consumoCasa1 * precoPorKwh;
     const valorCasa2 = (consumoTotal - consumoCasa1) * precoPorKwh;
 
+    // Formata em reais com vírgula
     document.getElementById("resultadoCasa1").innerText =
-      `Casa 1 deve pagar: R$ ${valorCasa1.toFixed(2)}`;
+      `Casa 1 deve pagar: R$ ${valorCasa1.toFixed(2).replace('.', ',')}`;
     document.getElementById("resultadoCasa2").innerText =
-      `Casa 2 deve pagar: R$ ${valorCasa2.toFixed(2)}`;
+      `Casa 2 deve pagar: R$ ${valorCasa2.toFixed(2).replace('.', ',')}`;
   } else {
     document.getElementById("resultadoCasa1").innerText = "Dados inválidos";
     document.getElementById("resultadoCasa2").innerText = "";
   }
+}
+
+function limpar() {
+  document.getElementById("valorConta").value = "";
+  document.getElementById("consumoTotal").value = "";
+  document.getElementById("consumoCasa1").value = "";
+  document.getElementById("resultadoCasa1").innerText = "";
+  document.getElementById("resultadoCasa2").innerText = "";
 }
